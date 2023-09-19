019fbf67486e2cd638a13bfde788d66ca6920e7c
BarteXikora <sbartek50@gmail.com>
2023-09-19 15:00:00 +0200

    Created button component.

---
+++ src/components/elements/Button.styles.tsx
@@ -0,0 +1,61 @@
+import styled from 'styled-components'
+
+type StyledButtonProps = {
+    variant?: 'primary' | 'secondary' | 'tertiary' | 'menu'
+    size?: 'big' | 'small'
+}
+
+const StyledButton = styled.button<StyledButtonProps>`
+    background-color: ${(props) => (props.variant === 'secondary' || props.variant === 'tertiary') ?
+        'transparent'
+        :
+        props.variant === 'menu' ?
+            props.theme.colors.primaryDarker
+            :
+            props.theme.colors.primary
+    };
+
+    padding: ${(props) => props.size === 'small' ? '8px 18px' : '12px 20px'};
+
+    border: 1px solid ${(props) => (props.variant === 'tertiary' || props.variant === 'menu') ?
+        'transparent'
+        :
+        props.theme.colors.white
+    };
+
+    border-radius: 100px;
+    color: ${(props) => props.theme.colors.white};
+    font-size: ${(props) => props.theme.fontSizes.default};
+    text-align: left;
+    display: inline-flex;
+    align-items: center;
+    gap: 8px;
+    line-height: 25px;
+    cursor: pointer;
+    transition: all ease-in-out .2s;
+
+    &:hover {
+        background-color: ${(props) => (props.variant === 'secondary' || props.variant === 'menu') ?
+        props.theme.colors.primary
+        :
+        props.variant !== 'tertiary' ?
+            props.theme.colors.primaryLighter
+            :
+            props.theme.colors.primary
+    };
+    }
+
+    &:active {
+        background-color: ${(props) => (props.variant === 'primary' || props.variant === undefined) ?
+        props.theme.colors.primary
+        :
+        props.theme.colors.primaryDarker
+    };
+    }
+
+    img {
+        height: 25px;
+    }
+`
+
+export default StyledButton
--- src/components/layout/sideMenu/SideMenu.tsx
+++ src/components/layout/sideMenu/SideMenu.tsx
@@ -1,9 +1,19 @@
 import StyledSideMenu from './SideMenu.styles'
 import logo from '../../../assets/img/img-logo.png'
 
+import Button from '../../elements/Button'
+
+import icon from '../../../assets/icons/icon-folder.png'
+
 const SideMenu = () => {
     return <StyledSideMenu>
         <img src={logo} alt="logo multishare" className='logo' />
+
+        <Button>
+            <img src={icon} alt="" />
+
+            Wszystkie pliki
+        </Button>
     </StyledSideMenu>
 }
 
