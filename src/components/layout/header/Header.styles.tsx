abd813bc0ba48e70e578439739040f7f531fd25b
BarteXikora <sbartek50@gmail.com>
2023-09-29 12:42:44 +0200

    Created path display in header component.

--- src/components/layout/header/Header.styles.tsx
+++ src/components/layout/header/Header.styles.tsx
@@ -2,6 +2,60 @@ import styled from 'styled-components'
 
 const StyledHeader = styled.header`
     background-color: ${(props) => props.theme.colors.gray};
+    padding: ${(props) => props.theme.margins.sectionBig};
+
+    .path-box {
+        padding: ${(props) => props.theme.margins.YSmall} 0;
+        padding-left: 25px;
+        position: relative;
+        font-weight: bold;
+
+        &::after {
+            position: absolute;
+            content: '';
+            width: 9px;
+            height: 100%;
+            background-color: ${(props) => props.theme.colors.primary};
+            top: 0;
+            left: 0;
+            border-radius: 1000px;
+        }
+
+        h1 {
+            margin: 0;
+            font-size: ${(props) => props.theme.fontSizes.title};
+            cursor: default;
+        }
+
+        a {
+            color: ${(props) => props.theme.colors.white};
+            text-decoration: none;
+            transition: ${(props) => props.theme.transition};
+
+            &:hover {
+                color: ${(props) => props.theme.colors.primaryLighter};
+            }
+        }
+
+        .arrow {
+            position: relative;
+            display: inline-block;
+            width: 30px;
+
+            &::after {
+                position: absolute;
+                content: '';
+                width: 12px;
+                height: 12px;
+                top: -14px;
+                left: 5px;
+                transform: rotateZ(45deg);
+                border-top: 2px solid ${(props) => props.theme.colors.white};
+                border-right: 2px solid ${(props) => props.theme.colors.white};
+                border-radius: 0 4px 0 0;
+            }
+        }
+    }
 `
 
 export default StyledHeader
--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,7 +1,24 @@
 import StyledHeader from './Header.styles'
 
+type __currentPathProps = string[]
+
+const __curentPath: __currentPathProps = ['Moje pliki', 'Prywatne', 'Obrazy', 'Wycieczka sierpień 2023']
+
 const Header = () => {
-    return <StyledHeader>header</StyledHeader>
+    return <StyledHeader>
+        <div className="path-box">
+            <span>Ścieżka:</span>
+
+            <h1>
+                {
+                    __curentPath.map((path, n) => {
+                        if (n + 1 === __curentPath.length) return <>{path} <span className='arrow' /></>
+                        return <><a href='#'>{path}</a> <span className='arrow' /> </>
+                    })
+                }
+            </h1>
+        </div>
+    </StyledHeader>
 }
 
 export default Header
