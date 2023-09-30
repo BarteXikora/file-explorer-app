29aaa3b0e36cd3c4aeea373eda9ef5723e6d188b
BarteXikora <sbartek50@gmail.com>
2023-09-30 10:30:20 +0200

    Created folder component styles.

--- src/components/elements/folder/Folder.styles.tsx
+++ src/components/elements/folder/Folder.styles.tsx
@@ -1,6 +1,48 @@
 import styled from 'styled-components'
 
 const StyledFolder = styled.div`
-    
+    position: relative;
+    display: flex;
+    align-items: center;
+    background-color: ${(props) => props.theme.colors.gray};
+    padding: ${(props) => props.theme.margins.YSmall} ${(props) => props.theme.margins.XNormal};
+    padding-right: 0;
+    white-space: nowrap;
+    border-radius: 1000px;
+    overflow: hidden;
+    transition: ${(props) => props.theme.transition};
+    cursor: pointer;
+
+    &:hover {
+        background-color: ${(props) => props.theme.colors.grayLighter};
+
+        &::after {
+            background-color: ${(props) => props.theme.colors.grayLighter};
+        }
+    }
+
+    &::after {
+        position: absolute;
+        content: '';
+        height: 100%;
+        width: 50px;
+        background-color: ${(props) => props.theme.colors.gray};
+        right: 0;
+        z-index: 0;
+        transition: ${(props) => props.theme.transition};
+    }
+
+    img {
+        margin-right: ${(props) => props.theme.margins.XSmall};
+    }
+
+    button {
+        position: absolute;
+        padding: ${(props) => props.theme.margins.YSmall};
+        height: 100%;
+        margin-left: auto;
+        right: 0;
+        z-index: 10;
+    }
 `
 export default StyledFolder
--- src/components/elements/folder/Folder.tsx
+++ src/components/elements/folder/Folder.tsx
@@ -1,4 +1,8 @@
 import StyledFolder from './Folder.styles'
+import Button from '../../ui/button/Button'
+
+import iconFolder from '../../../assets/icons/icon-folder.png'
+import iconMenu from '../../../assets/icons/icon-menu-dots.png'
 
 type FolderProps = {
     name: string
@@ -6,7 +10,13 @@ type FolderProps = {
 }
 
 const Folder = ({ name, star }: FolderProps) => {
-    return <StyledFolder role='button'>{name} {star && '*'}</StyledFolder>
+    return <StyledFolder role='button'>
+        <img src={iconFolder} alt="Folder" />
+
+        {name}
+
+        <Button variant='tertiary'><img src={iconMenu} alt="Folder" /></Button>
+    </StyledFolder>
 }
 
 export default Folder
