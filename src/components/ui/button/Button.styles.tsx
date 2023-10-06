0a763ddc9a20b849ce585d8ba576ca65a834473e
BarteXikora <sbartek50@gmail.com>
2023-10-06 15:23:07 +0200

    Exported styled button props.

--- src/components/ui/button/Button.styles.tsx
+++ src/components/ui/button/Button.styles.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-type StyledButtonProps = {
+export type StyledButtonProps = {
     variant?: 'primary' | 'secondary' | 'tertiary' | 'menu'
     size?: 'big' | 'small'
 }
