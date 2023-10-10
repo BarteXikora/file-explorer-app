cf8cb7c9305443eb10604dc4e5a57ac6e8e3f2d7
BarteXikora <sbartek50@gmail.com>
2023-10-10 16:12:57 +0200

    Created content tools components content.

--- src/components/layout/contentTools/ContentTools.styles.tsx
+++ src/components/layout/contentTools/ContentTools.styles.tsx
@@ -7,6 +7,26 @@ const StyledContentTools = styled.div`
     padding: ${(props) => props.theme.margins.small + ' ' + props.theme.margins.big};
     top: 309px;
     background-color: ${(props) => props.theme.colors.grayDarker};
+    display: flex;
+    align-items: center;
+    gap: ${(props) => props.theme.margins.small};
+    z-index: 1100;
+
+    h2 {
+        display: block;
+        width: 25%;
+        font-size: ${(props) => props.theme.fontSizes.title};
+        margin: 0;
+        color: ${(props) => props.theme.colors.grayLighter};
+        white-space: nowrap;
+        overflow: hidden;
+    }
+
+    .separator {
+        margin: 0 ${(props) => props.theme.margins.small};
+        height: 25px;
+        border: 1px solid ${(props) => props.theme.colors.white};
+    }
 `
 
 export default StyledContentTools
--- src/components/layout/contentTools/ContentTools.tsx
+++ src/components/layout/contentTools/ContentTools.tsx
@@ -1,7 +1,50 @@
+import { useState } from 'react'
 import StyledContentTools from './ContentTools.styles'
+import Checkbox from '../../ui/checkbox/Checkbox'
+import Button from '../../ui/button/Button'
+
+import iconDownload from '../../../assets/icons/icon-download.png'
+import iconMove from '../../../assets/icons/icon-move.png'
+import iconStar from '../../../assets/icons/icon-star.png'
+import iconShow from '../../../assets/icons/icon-show.png'
+import iconDelete from '../../../assets/icons/icon-delete.png'
+
+const __currentFolderName = 'Wycieczka na rowery -  sierpień 2023'
 
 const ContentTools = () => {
-    return <StyledContentTools>content tools</StyledContentTools>
+    const [__isAllSelected, __setIsAllSelected] = useState(false)
+
+    return <StyledContentTools>
+        <h2>{__currentFolderName}:</h2>
+
+        <div className="separator"></div>
+
+        <Checkbox isChecked={__isAllSelected} setIsChecked={__setIsAllSelected} label='Zaznacz wszystko' />
+
+        <div className="separator"></div>
+
+        <div className="tools-buttons">
+            <Button variant='tertiary' size='small'>
+                <img src={iconDownload} alt="Pobierz" />
+            </Button>
+
+            <Button variant='tertiary' size='small'>
+                <img src={iconMove} alt="Przenieś do..." />
+            </Button>
+
+            <Button variant='tertiary' size='small'>
+                <img src={iconStar} alt="Oznacz gwiazdką" />
+            </Button>
+
+            <Button variant='tertiary' size='small'>
+                <img src={iconShow} alt="Pokaż / ukryj" />
+            </Button>
+
+            <Button variant='tertiary' size='small'>
+                <img src={iconDelete} alt="Usuń" />
+            </Button>
+        </div>
+    </StyledContentTools>
 }
 
 export default ContentTools
