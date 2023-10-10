9b2d5ea38b3943d4c2942767b3f7bf7ac6f95595
BarteXikora <sbartek50@gmail.com>
2023-10-10 16:24:16 +0200

    Created function to shorten folder names.

---
+++ src/functions/shortName/shortName.ts
@@ -0,0 +1,14 @@
+const shortName = (name: string, maxLength: number): string => {
+    if (name.length <= maxLength) return name
+
+    let newName = name.substring(0, maxLength - 3)
+
+    while (newName.length > 0 && newName[newName.length - 1] === ' ')
+        newName = newName.substring(0, newName.length - 1)
+
+    newName += '...'
+
+    return newName
+}
+
+export default shortName
