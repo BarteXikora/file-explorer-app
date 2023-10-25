c9f06b8b4d6b04be2ef4f4478d5c6a901079f227
BarteXikora <sbartek50@gmail.com>
2023-10-25 09:14:44 +0200

    Prepared content slice.

---
+++ src/store/features/__initialState.ts
@@ -0,0 +1,84 @@
+export type folderType = {
+    id: number,
+    name: string,
+    content: contentType
+}
+
+export type fileType = {
+    id: number,
+    name: string,
+    extension: string
+}
+
+export type contentType = {
+    folders?: folderType[],
+    files?: fileType[]
+}
+
+export type initialStateType = {
+    projectName: string,
+    currentFolder: contentType,
+    content: contentType
+}
+
+const initialState: initialStateType = {
+    projectName: 'Moje pliki',
+    currentFolder: {},
+    content: {
+        folders: [
+            {
+                id: 0,
+                name: 'Prywatne',
+                content: {
+                    folders: [
+                        {
+                            id: 3,
+                            name: 'Obrazy',
+                            content: {
+                                folders: [
+                                    {
+                                        id: 5,
+                                        name: 'Wycieczka na rowery - sierpień 2023',
+                                        content: {
+                                            folders: [
+                                                {
+                                                    id: 6,
+                                                    name: 'Nieobrobione',
+                                                    content: {}
+                                                }, {
+                                                    id: 7,
+                                                    name: 'Pozostałe - większość do śmieci',
+                                                    content: {}
+                                                }
+                                            ],
+                                            files: [
+                                                {
+                                                    id: 100,
+                                                    name: 'Wycieczka na rowery 1',
+                                                    extension: 'PNG',
+                                                }
+                                            ]
+                                        }
+                                    }
+                                ],
+                                files: []
+                            }
+                        }, {
+                            id: 4,
+                            name: 'Dokumenty',
+                            content: {}
+                        }
+                    ],
+                    files: []
+                }
+            }, {
+                id: 1,
+                name: 'Pozostałe',
+                content: {}
+            }
+        ],
+        files: []
+    }
+}
+
+export default initialState
---
+++ src/store/features/contentSlice.ts
@@ -0,0 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit'
+import initialState from './__initialState'
+
+export const contentSlice = createSlice({
+    name: 'contentSlice',
+    initialState,
+    reducers: {}
+})
+
+export default contentSlice.reducer
+// export const {} = contentSlice.actions
