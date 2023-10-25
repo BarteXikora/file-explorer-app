71e4f9a50749855961fac7144a02c3b484307c35
BarteXikora <sbartek50@gmail.com>
2023-10-25 13:46:00 +0200

    Used content store in path component, so it shows an actual path.

--- src/components/elements/path/Path.tsx
+++ src/components/elements/path/Path.tsx
@@ -1,31 +1,40 @@
+import { useState, useEffect } from 'react'
+import { useSelector } from '../../../store/store'
+
 import StyledPath from './Path.styles'
 import PathPart from './pathPart/PathPart'
 import CollapsePathButton from './collapsePathButton/CollapsePathButton'
 
 const MAX_FOLDER_NAME_LENGTH = 22
 
-type __currentPathProps = string[]
-const __curentPath: __currentPathProps = ['Moje pliki', 'Prywatne', 'Obrazy', 'Wycieczka na rowery -  sierpień 2023']
-
 const Path = () => {
+    const projectName = useSelector(state => state.content.projectName)
+    const currentPath = useSelector(state => state.content.currentPath)
+
+    const [localPath, setLocalPath] = useState<string[]>([projectName, ...currentPath])
+    useEffect(() => {
+        setLocalPath([projectName, ...currentPath])
+
+    }, [projectName, currentPath])
+
     return <StyledPath>
         <span>Ścieżka:</span>
 
         <h1>
             <CollapsePathButton
-                content={__curentPath}
-                isPathCollapsed={__curentPath.length >= 5}
+                content={localPath}
+                isPathCollapsed={localPath.length >= 5}
                 maxFolderNameLength={MAX_FOLDER_NAME_LENGTH}
             />
 
             <div className="path-links">
                 {
-                    __curentPath.map((path, n) => {
-                        if ((__curentPath.length >= 5 && n > __curentPath.length - 4) || __curentPath.length < 5)
+                    localPath.map((path, n) => {
+                        if ((localPath.length >= 5 && n > localPath.length - 4) || localPath.length < 5)
                             return <PathPart
                                 key={n} path={path}
                                 location='#'
-                                isLast={n === __curentPath.length - 1}
+                                isLast={n === localPath.length - 1}
                                 maxFolderNameLength={MAX_FOLDER_NAME_LENGTH}
                             />
 
