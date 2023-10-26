064cfc8ee4423a417df31f9890b79743baf71a01
BarteXikora <sbartek50@gmail.com>
2023-10-26 20:40:42 +0200

    Used useChangePath hook in path and collapse path button components.

--- src/components/elements/path/Path.tsx
+++ src/components/elements/path/Path.tsx
@@ -1,47 +1,31 @@
-import { useState, useEffect } from 'react'
-import { useSelector, useDispatch } from '../../../store/store'
-import { setPath } from '../../../store/features/contentSlice/contentSlice'
-
 import StyledPath from './Path.styles'
 import PathPart from './pathPart/PathPart'
 import CollapsePathButton from './collapsePathButton/CollapsePathButton'
 
+import useChangePath from '../../../functions/useChangePath/useChangePath'
+
 const MAX_FOLDER_NAME_LENGTH = 22
 
 const Path = () => {
-    const projectName = useSelector(state => state.content.projectName)
-    const currentPath = useSelector(state => state.content.currentPath)
-
-    const dispatch = useDispatch()
-
-    const [localPath, setLocalPath] = useState<string[]>([projectName, ...currentPath])
-    useEffect(() => {
-        setLocalPath([projectName, ...currentPath])
-
-    }, [projectName, currentPath])
-
-    const navigateToPath = (path: string[]) => {
-        dispatch(setPath(path))
-    }
+    const { fullPath, changePath } = useChangePath()
 
     return <StyledPath>
         <span>Ścieżka:</span>
 
         <h1>
             <CollapsePathButton
-                content={localPath}
-                isPathCollapsed={localPath.length >= 5}
+                isPathCollapsed={fullPath.length >= 5}
                 maxFolderNameLength={MAX_FOLDER_NAME_LENGTH}
             />
 
             <div className="path-links">
                 {
-                    localPath.map((path, n) => {
-                        if ((localPath.length >= 5 && n > localPath.length - 4) || localPath.length < 5)
+                    fullPath.map((path, n) => {
+                        if ((fullPath.length >= 5 && n > fullPath.length - 4) || fullPath.length < 5)
                             return <PathPart
                                 key={n} path={path}
-                                click={() => navigateToPath(localPath.slice(1, n + 1))}
-                                isLast={n === localPath.length - 1}
+                                click={() => changePath(fullPath.slice(1, n + 1))}
+                                isLast={n === fullPath.length - 1}
                                 maxFolderNameLength={MAX_FOLDER_NAME_LENGTH}
                             />
 
--- src/components/elements/path/collapsePathButton/CollapsePathButton.tsx
+++ src/components/elements/path/collapsePathButton/CollapsePathButton.tsx
@@ -1,23 +1,18 @@
-import { useDispatch } from '../../../../store/store'
-import { setPath } from '../../../../store/features/contentSlice/contentSlice'
-
 import Dropdown from '../../../ui/dropdown/Dropdown'
 import Button from '../../../ui/button/Button'
 
-import shortName from '../../../../functions/shortName/shortName'
 import iconFolder from '../../../../assets/icons/icon-folder.png'
 
+import shortName from '../../../../functions/shortName/shortName'
+import useChangePath from '../../../../functions/useChangePath/useChangePath'
+
 type CollapsePathButtonProps = {
-    content: string[]
     isPathCollapsed: boolean
     maxFolderNameLength: number
 }
 
-const CollapsePathButton = ({ content, isPathCollapsed, maxFolderNameLength }: CollapsePathButtonProps) => {
-    const dispatch = useDispatch()
-    const navigateToPath = (path: string[]) => {
-        dispatch(setPath(path))
-    }
+const CollapsePathButton = ({ isPathCollapsed, maxFolderNameLength }: CollapsePathButtonProps) => {
+    const { fullPath, changePath } = useChangePath()
 
     return <div className={`path-dropdown ${isPathCollapsed && 'collapsed'}`}>
         <Dropdown
@@ -25,12 +20,12 @@ const CollapsePathButton = ({ content, isPathCollapsed, maxFolderNameLength }: C
             buttonOptions={{ size: 'small' }}
             dropdownContent={<>
                 {
-                    content.map((path, n) => {
+                    fullPath.map((path, n) => {
                         return <Button
                             key={n}
                             variant='tertiary'
                             size='small'
-                            onClick={() => navigateToPath(content.slice(1, n + 1))}
+                            onClick={() => changePath(fullPath.slice(1, n + 1))}
                         >
                             <img src={iconFolder} alt="Folder" />
 
